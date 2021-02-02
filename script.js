55644f2f3ff66a9676ed1c6cf1bdefa9a099b1e7
Plasnid <stuart@sackler.ca>
2021-02-01 22:40:33 -0500

    initial file upload

---
+++ script.js
@@ -0,0 +1,51 @@
+let p1 = new Promise((resolve, reject) =>{
+    let a = 1 + 1;
+    if(a==2){
+        resolve('egad!  I worked!  I will never ride a hamster to work!');
+    }else{
+        reject('darn it! no pony....hamster time forever!');
+    }
+});
+
+let p2 = new Promise((resolve, reject) =>{
+    let a = 1 + 1;
+    if(a==2){
+        resolve('egad!  I worked!  I will never ride a hamster to work!');
+    }else{
+        reject('darn it! no pony....hamster time forever!');
+    }
+});
+
+Promise.all([p1,p2]).then(function(){
+    console.log("all the parts are ready, lets go!");
+}).catch(function(){
+    console.log(`not yet ready!  something is broken`);
+});
+
+/*p1.then((message) =>{
+    console.log(`success: ${message}`);
+}).catch((message)=>{
+    console.log(`fail: ${message}`);
+})*/
+
+function bagCheck(contents){
+    if(contents!="contraban"){
+        return new Promise(resolve =>{
+            setTimeout(() =>{
+                resolve("free to go");
+            },3000)
+        });
+    }else{
+        return new Promise(reject =>{
+            setTimeout(()=>{
+                reject("traveller detained");
+            },5000);
+        })
+    }
+}
+async function goingThroughCustoms(){
+    console.log('going through customs, lets get our bags checked');
+    const result = await bagCheck("not cobb nuts");
+    console.log(result);
+}
+goingThroughCustoms();
